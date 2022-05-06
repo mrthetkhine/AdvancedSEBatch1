7cf5b49714ffed65ae4cdab84521cdacc28ea6ff
Thet Khine <mrthetkhine@gmail.com>
2022-05-06 17:06:38 +0630

    Add Design Pattern

---
+++ DesignPattern/designpattern/src/main/java/com/turing/oop/designpattern/App.java
@@ -0,0 +1,13 @@
+package com.turing.oop.designpattern;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
